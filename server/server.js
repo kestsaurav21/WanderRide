06a8f8fc5f3ca78b090e5b328a727c3ec875fb7a
kestsaurav21 <kestsaurav21@outlook.com>
2025-10-28 18:20:28 +0530

    feat: "Server connected with DB"

---
+++ server/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const connectDB = require('./config/db');
+const app = require('./app')
+
+
+const PORT = process.env.PORT || 7700;
+
+connectDB().then(() => {
+  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+})
