5ca88389a9accd26b2bf5eea44c19658121de904
kestsaurav21 <kestsaurav21@outlook.com>
2025-10-28 22:23:53 +0530

    update

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
 //Middlewares
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/users", userRoutes);
 
 module.exports = app;
