5af37e35c3d36ecf51a75ca6569668c3cce9f326
kestsaurav21 <kestsaurav21@outlook.com>
2025-10-30 01:27:00 +0530

    update

--- server/routes/vehicleRoutes.js
+++ server/routes/vehicleRoutes.js
@@ -1,13 +1,10 @@
-const express = require('express');
-const { addVehicle } = require('../controllers/vehicleController');
-const authMiddleware = require('../middlewares/authMiddleware');
-const adminMiddleware = require('../middlewares/adminMiddleware');
-
+const express = require("express");
+const { addVehicle } = require("../controllers/vehicleController");
+const authMiddleware = require("../middlewares/authMiddleware");
+const adminMiddleware = require("../middlewares/adminMiddleware");
 
 const router = express.Router();
 
-
-
-router.post('/addVehicle', authMiddleware, adminMiddleware,  addVehicle);
+router.post("/addVehicle", authMiddleware, adminMiddleware, addVehicle);
 
 module.exports = router;
