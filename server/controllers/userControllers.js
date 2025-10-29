5e18949fd699c0c049c2b8653b209b5889c5b793
kestsaurav21 <kestsaurav21@outlook.com>
2025-10-29 21:42:01 +0530

    fix: loginUser

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,7 +1,7 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 
-exports.registerUser = async (req, res) => {
+const registerUser = async (req, res) => {
   const { email, password, name } = req.body;
 
   try {
@@ -32,7 +32,7 @@ exports.registerUser = async (req, res) => {
         id: newUser._id,
         name: newUser.name,
         email: newUser.email,
-        password: newUser.password
+        password: newUser.password,
       },
     });
   } catch (error) {
@@ -43,6 +43,57 @@ exports.registerUser = async (req, res) => {
   }
 };
 
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    //check the user exists
+    const existingUser = await User.findOne({ email });
 
+    if (!existingUser) {
+      res.status(404).json({
+        success: false,
+        message: "❌ User not found. Please register first.",
+      });
+    }
+    //compare the entered password with hashpassword
+    const isMatch = await bcrypt.compare(password, existingUser.password);
+    if (!isMatch) {
+      return res.status(404).json({
+        success: false,
+        message: "🔴 Invalid credentials. Please try again.",
+      });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign(
+      {
+        id: existingUser._id,
+        email: existingUser.email,
+        role: existingUser.role,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN || "1h" }
+    );
+
+    //If password matches
+
+    res.status(200).json({
+      success: true,
+      message: "✅ Login successful!",
+      token,
+      user: {
+        id: existingUser._id,
+        name: existingUser.name,
+        email: existingUser.email,
+      },
+    });
+  } catch (error) {
+    console.error("Error logging in:", error);
+    res.status(500).json({
+      success: false,
+      message: "Server error. Please try again later.",
+    });
+  }
+};
 
 module.exports = { registerUser, loginUser };
