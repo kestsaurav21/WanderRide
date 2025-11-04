190f6d3918bca77565d2ca178943ad2c968da911
kestsaurav21 <kestsaurav21@outlook.com>
2025-11-04 18:13:11 +0530

    feat(vehicle): add addVehicle route and controller

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
+const vehicleRoutes = require("./routes/vehicleRoutes")
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
+app.use("/vehicle", vehicleRoutes);
 
 module.exports = app;
--- server/routes/vehicleRoutes.js
+++ server/routes/vehicleRoutes.js
@@ -1,31 +1,11 @@
-const express = require("express");
-
-
-// Controllers
-
-const { addVehicle, getAllVehicle, updateVehicle } = require("../controllers/vehicleController");
-
-
-
-// Middlewares
-
-const authMiddleware = require("../middlewares/authMiddleware");
-const adminMiddleware = require("../middlewares/adminMiddleware");
+const express = require('express');
+const isAuthenticated = require('../middlewares/authMiddleware');
+const isAdmin = require('../middlewares/roleMiddleware');
+const { addVehicle } = require('../controllers/vehicleController');
 
 const router = express.Router();
 
-
-// ✅ Routes
-
-
-// Add new vehicle (only admin)
-router.post("/addVehicle", authMiddleware, adminMiddleware, addVehicle);
-
-//Get all vehicles (open for all logged-in users)
-router.get('/getAllVehicles', authMiddleware, getAllVehicle)
-
-//Update the vehicle(only admin)
-router.put('/updateVehicle/:id', authMiddleware, adminMiddleware, updateVehicle)
+router.post('/add', isAuthenticated, isAdmin, addVehicle);
 
 
 
