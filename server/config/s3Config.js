64ffd481033daa9c47b30cc5b778854dd2549f0d
kestsaurav21 <kestsaurav21@outlook.com>
2025-11-05 16:36:58 +0530

    feat: Configure and export AWS S3 client

---
+++ server/config/s3Config.js
@@ -0,0 +1,10 @@
+const AWS = require("aws-sdk")
+
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_ACCESS_KEY_ID,
+    region: process.env.AWS_REGION,
+})
+
+const s3 = new AWS.S3();
+module.exports = s3;
